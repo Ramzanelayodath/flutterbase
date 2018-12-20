98597f14c5249556553033aef7e921cd03b1b7e2
DESKTOP-5P81L5J\Beegains <ramzanelayodath@gmail.com>
2018-12-20 12:30:39 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,125 +12,40 @@ class MyApp extends StatelessWidget {
       title: 'Unit Converter',
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Login"),
+          title: Text("Beegains"),
           backgroundColor: Colors.lightBlue,
         ),
-        body: HelloRectangle(),
+        body: Listview(),
       ),
     );
   }
 }
 
-class HelloRectangle extends StatefulWidget
+class Listview extends StatelessWidget
 {
   @override
-  State<StatefulWidget> createState() {
+  Widget build(BuildContext context) {
+    return Center(
+      child: ListView(
+        children: <Widget>[
+          Text("Ramzan",style: TextStyle(fontSize: 25.0)),
+          Text("Shani",style: TextStyle(fontSize: 25.0)),
+          Text("Mukund",style: TextStyle(fontSize: 25.0)),
+          Text("Yaseen",style: TextStyle(fontSize: 25.0)),
+          Text("Fansad",style: TextStyle(fontSize: 25.0)),
+          Text("Tinson",style: TextStyle(fontSize: 25.0)),
+          Text("Alphonsa",style: TextStyle(fontSize: 25.0)),
+          Text("Riyas",style: TextStyle(fontSize: 25.0))
 
-    return HelloRectanglestate();
+        ],
+      )
+    );
   }
 
 }
 
-  class HelloRectanglestate extends State<HelloRectangle> {
-    BuildContext context ;
-    String word = "hi";
-
-    @override
-    Widget build(BuildContext context) {
-      context = context;
-      return Center(
-          child: Column(
-            children: <Container>[
-              Container(
-                  margin: EdgeInsets.all(10),
-                  child: TextField(
-                    controller: usernamecontroller,
-                    decoration: InputDecoration(
-                      hintText: 'Username',
-                      labelStyle: new TextStyle(color: const Color(0xFF424242)),
-                    ),
 
 
-                  )
-              ),
-              Container(
-                  margin: EdgeInsets.all(10),
-                  child: TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: 'Password',
-                      labelStyle: new TextStyle(color: const Color(0xFF424242)),
-
-                    ),
-
-
-                  )
-              ),
-              Container(
-                  margin: EdgeInsets.all(10),
-                  child: new SizedBox(
-                      width: double.infinity,
-                      height: 50.0,
-                      child: RaisedButton(
-                        child: const Text('LOGIN',
-                            style: TextStyle(color: Colors.white)),
-                        color: Colors.lightBlue,
-                        elevation: 4.0,
-                        splashColor: Colors.blueGrey,
-                        onPressed: () {
-                           // showAlert(usernamecontroller.text, context);
-                          //showSnackBar(usernamecontroller.text, context);
-                          setState(() {
-                            word = usernamecontroller.text;
-                          });
-                          // Perform some action
-                        })
-                  )
-              ),
-
-              Container(
-                  margin: EdgeInsets.all(10),
-                  child: new SizedBox(
-                      width: double.infinity,
-                      height: 50.0,
-                      child: Text("$word")
-                  )
-              )
-
-            ],
-          )
-      );
-    }
-    void showAlert(String username,BuildContext context)
-    {
-     /* showDialog(
-          context: context,
-          builder: (_) => new AlertDialog(
-            title: new Text("Username"),
-            content: new Text(username),
-          )
-      ); */
-      showDialog(context: context, child:
-      new AlertDialog(
-        title: new Text("Username"),
-        content: new Text(username),
-        actions: <Widget>[
-          FlatButton(onPressed:(){
-            Navigator.pop(context);
-          }, child: Text("OK"))
-        ],
-
-      )
-      );
-    }
-
-    void showSnackBar(String username,BuildContext context)
-    {
-      final snackBar = SnackBar(content: Text(username));
-      Scaffold.of(context).showSnackBar(snackBar);
-
-    }
-}
 
 
 
